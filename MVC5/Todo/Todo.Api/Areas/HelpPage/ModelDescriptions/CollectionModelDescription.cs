6398d590c8ac30de920d550a1bc7b67a8add08e1
ppk11654hotmail <ppk11654@hotmail.com>
2015-08-14 22:48:15 +0800

    delete
    delete

--- MVC5/Todo/Todo.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Todo.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
